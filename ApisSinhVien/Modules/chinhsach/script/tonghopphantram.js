2ac1d94c9fc46992b99f68b9e3537cd11508d04c
Azz <vanhieptn95@gmail.com>
2026-04-17 19:29:02 +0700

    Update policy object keys and enhance decision form layout

--- ApisSinhVien/Modules/chinhsach/script/tonghopphantram.js
+++ ApisSinhVien/Modules/chinhsach/script/tonghopphantram.js
@@ -983,7 +983,7 @@ TongHopPhanTram.prototype = {
             'dSoThang': $(point).val(),
             'strDiem_KieuHoc_Id': edu.util.getValById('dropSearch_KieuHoc'),
             'strTaiChinh_CacKhoanThu_Id': edu.util.getValById('dropSearch_KhoanThu'),
-            'strDoiTuongChinhSach_Id': edu.system.getValById('dropSearch_DoiTuong'),
+            'strDoiTuongChinhSach_Id': edu.system.getValById('dropSearch_CheDo'),
             'strNguoiThucHien_Id': edu.system.userId,
         };
         edu.system.makeRequest({
